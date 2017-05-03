b66fd374ca3b20e14311af3c5fb80dcf298203d1
Carl Hultberg <carl.johan.hultberg@gmail.com>
2017-05-03 22:09:38 +0200

    Example skeleton code
    Såhär skulle ratingfunktionen vara uppbyggd. Väldigt basic.

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from yahoo_finance import Share
 
-yahoo = Share('SAS.ST')
-print(yahoo.get_open())
+yahoo = Share('GOOG')
+
+def rater():
+    rate = 0
+    if (float(yahoo.get_price_earnings_ratio()) < 10):
+        rate += 1
+    if (float(yahoo.get_price_book()<1) or yahoo.get_price_book() < yahoo.get_price_earnings_ratio()):
+        rate += 1
+    if (float(yahoo.get_50day_moving_avg()) < yahoo.get_200day_moving_avg()):
+        rate += 1
+    else:
+        None
+    print(rate)
+
+from pprint import pprint
+pprint(yahoo.get_price_earnings_ratio())
+pprint(yahoo.get_price_book())
+pprint(yahoo.get_50day_moving_avg())
+pprint(yahoo.get_200day_moving_avg())
+
+rater()
